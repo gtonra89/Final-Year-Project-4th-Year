e69758c236e89ac681de69939069ca44495417d5
Ian Burke <iano.burke@gmail.com>
2018-01-17 22:36:20 +0000

    Added current temputure api

--- Statflow/app.py
+++ Statflow/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template 
-
+from flask import Flask, render_template, request 
+import requests
 # Pass in __name__ to help flask determine root path
 app = Flask(__name__) # create the application instance
 
@@ -9,8 +9,16 @@ app = Flask(__name__) # create the application instance
 def main():
     return render_template("index.html")
 
+@app.route('/temputure', methods=['POST']) #connect a webpage. '/' is a root directory.
+def temputure():
+    req = request.get('http://api.openweathermap.org/data/2.5/weather?&APPID=a662d25dc808bf3a512b5aab0cbfa6e2&q=galway')
+    json_object = req.json()
+    temp = float(json_object['main']['temp'])
+    
+    return render_template("index.html", temp=temp)
+
 @app.route('/dashboard') #connect a webpage. '/' is a root directory.
-def data():
+def dashboard():
     return render_template("dashboard.html")
 
 
