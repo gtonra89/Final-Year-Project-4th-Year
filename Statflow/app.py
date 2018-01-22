8780ca192e1412e82cf1bd0153cedd5946e905e9
Ian Burke <iano.burke@gmail.com>
2018-01-22 16:23:39 +0000

    Clean up

--- Statflow/app.py
+++ Statflow/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request 
 import requests
+
 # Pass in __name__ to help flask determine root path
 app = Flask(__name__) # create the application instance
+
+
 
 # Routing/Mapping
 # @ signifies a decorator which is a way to wrap a function and modify its behaviour
 @app.route('/') #connect a webpage. '/' is a root directory.
 def main():
+    
     return render_template("index.html")
 
 @app.route('/temputure', methods=['POST']) #connect a webpage. '/' is a root directory.
 def temputure():
-    req = request.get('http://api.openweathermap.org/data/2.5/weather?&APPID=a662d25dc808bf3a512b5aab0cbfa6e2&q=galway')
-    json_object = req.json()
-    temp = float(json_object['main']['temp'])
+    #city = request.form['city']
+   
+    #req = request.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a662d25dc808bf3a512b5aab0cbfa6e2&q=galway')
+    #json_object = req.json()
+    #temp = float(json_object['main']['temp'])
     
     return render_template("index.html", temp=temp)
 
